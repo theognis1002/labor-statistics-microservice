c53ad6e235ea4b768dc4903c574167ff7a8952ad
theognis1002 <theognis1002@gmail.com>
2021-04-07 01:13:37 -0400

    added aggs query and initial import script

--- app/views.py
+++ app/views.py
@@ -24,15 +24,69 @@ def es_test():
         request_timeout=30,
     )
     res = es.search(index="salary", body={"query": {"match_all": {}}})
-    print(res)
     return jsonify(res)
+
+
+@api.route("/stats", methods=["POST"])
+def get_salary_statistics():
+    json_body = request.get_json()
+    if json_body:
+        location = json_body.get("location")
+        job_title = json_body.get("job_title")
+    else:
+        location = request.args.get("location")
+        job_title = request.args.get("job_title")
+
+    query_filters = []
+
+    if location:
+        query_filters.append(
+            {"multi_match": {"query": location, "fields": ["city", "state"]}}
+        )
+
+    if job_title:
+        query_filters.append(
+            {"match": {"job_title": {"query": job_title, "fuzziness": "AUTO"}}}
+        )
+
+    es = Elasticsearch(
+        hosts=[{"host": "host.docker.internal", "port": 9200}],
+        connection_class=RequestsHttpConnection,
+        max_retries=30,
+        retry_on_timeout=True,
+        request_timeout=30,
+    )
+
+    res = es.search(
+        index="salary",
+        body={
+            "query": {
+                "bool": {"must": query_filters if query_filters else {"match_all": {}}}
+            },
+            "size": 0,
+            "aggs": {
+                "mean_salary": {"avg": {"field": "wage_rate"}},
+                "median_salary": {
+                    "percentiles": {"field": "wage_rate", "percents": [50]}
+                },
+                "salary_percentiles": {
+                    "percentiles": {"field": "wage_rate", "percents": [25, 75]}
+                },
+            },
+        },
+    )
+    result = {
+        "statistics": res["aggregations"],
+        "num_results": res["hits"]["total"]["value"],
+    }
+    return jsonify(result)
 
 
 @api.after_request
 def after_request(response):
     """ Logging every api request. """
     app.logger.info(
-        "%s [%s] %s %s %s %s %s %s %s",
+        "%s [%s] %s %s %s %s %s %s",
         request.remote_addr,
         datetime.utcnow(),
         request.method,
